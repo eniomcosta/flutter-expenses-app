3e6dfe3791383002cf18dc3933cb59edecd847a4
Enio Costa <enio.m.costa@gmail.com>
2021-03-07 21:16:01 -0300

    Created transaction list

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -12,6 +13,16 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+        id: "t1",
+        date: DateTime.now(),
+        title: "Novo Tênis de Corrida",
+        value: 310.76),
+    Transaction(
+        id: "t2", date: DateTime.now(), title: "Conta de Luz", value: 211.13),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +30,8 @@ class HomePage extends StatelessWidget {
         title: Text("Despesas pessoais"),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
             width: double.infinity,
@@ -27,8 +40,45 @@ class HomePage extends StatelessWidget {
               child: Text("Gráfico"),
             ),
           ),
-          Card(
-            child: Text("Lista de despesas"),
+          Column(
+            children: _transactions.map((t) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        t.value.toString(),
+                        style: TextStyle(
+                            color: Colors.purple,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    ),
+                    //TODO fonte do título 16, bold e cor da data cinza
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          t.title,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        Text(
+                          t.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),
